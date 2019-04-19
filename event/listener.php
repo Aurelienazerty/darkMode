739592f5711b7c4d602b802533b1b47b4208dda3
Aurelienazerty <Aurelienazerty@users.noreply.github.com>
2019-04-19 10:16:18 +0200

    Code style change

--- event/listener.php
+++ event/listener.php
@@ -19,21 +19,22 @@ class listener implements EventSubscriberInterface
 {
 	/** @var \phpbb\template\template */
 	protected $template;
-  /** @var \phpbb\user $user */
+	/** @var \phpbb\user $user */
 	protected $user;
 
 
 	/**
 	 * Constructor
 	 *
-	 * @param \phpbb\template\template	$config	Config object
+	 * @param \phpbb\template\template	$template Template object
+	 * @param \phpbb\user			$user User object
 	 * @return \Aurelienazerty\darkMode\event\listener
 	 * @access public
 	 */
 	public function __construct(\phpbb\template\template $template, \phpbb\user $user)
 	{
 		$this->template = $template;
-    $this->user = $user;
+		$this->user = $user;
 	}
 
 	static public function getSubscribedEvents()
@@ -63,7 +64,7 @@ class listener implements EventSubscriberInterface
 		$this->template->append_var('STYLE_DO_DARK', $styleDoDark);
 		$this->template->append_var('STYLE_DO_LIGHT', $styleDoLight);
     
-    $this->user->add_lang_ext('Aurelienazerty/darkMode', 'dark_mode');
+		$this->user->add_lang_ext('Aurelienazerty/darkMode', 'dark_mode');
 		$this->template->assign_var('DO_DARK_MESSAGE'	, $this->user->lang['DO_DARK_MODE']);
 		$this->template->assign_var('DO_LIGHT_MESSAGE'	, $this->user->lang['DO_LIGHT_MODE']);
 	}
