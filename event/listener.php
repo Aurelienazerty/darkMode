729403511f1175eee5725e534ce9580562426147
Aurelienazerty <Aurelienazerty@users.noreply.github.com>
2020-11-08 13:12:42 +0100

    1.0.2
    Code style change

--- event/listener.php
+++ event/listener.php
@@ -23,54 +23,63 @@ class listener implements EventSubscriberInterface
 	protected $user;
 	/** @var \phpbb\request\request */
 	protected $request;
+	/** @var \phpbb\config\config */
+	protected $config;
 
 
 	/**
 	 * Constructor
 	 *
 	 * @param \phpbb\template\template	$template Template object
-	 * @param \phpbb\user								$user User object
-	 * @param \phpbb\request\request		$request
+	 * @param \phpbb\user					$user User object
+	 * @param \phpbb\request\request		$request Request object
+	 * @param \phpbb\config\config			$config	Config object
 	 * @return \aurelienazerty\darkmode\event\listener
 	 * @access public
 	 */
-	public function __construct(\phpbb\template\template $template, \phpbb\user $user, \phpbb\request\request $request)
+	public function __construct(\phpbb\template\template $template, \phpbb\user $user, \phpbb\request\request $request,\phpbb\config\config $config)
 	{
 		$this->template = $template;
-		$this->user = $user;
-		$this->request = $request;
+		$this->user 	= $user;
+		$this->request 	= $request;
+		$this->config	= $config;
 	}
 
 	static public function getSubscribedEvents()
 	{
 		return array(
-			'core.page_header'			=> 'do_dark',
-			'core.adm_page_header'	=> 'do_dark',
+			'core.page_header'				=> 'do_dark',
+			'core.adm_page_header'		=> 'do_dark',
 		);
 	}
 	
 	public function do_dark($event)
 	{
-		$idDark = $this->request->variable('darkmode', false, true, \phpbb\request\request_interface::COOKIE);
-		if ($idDark)
+		$cookie_darkmode_name = $this->config['cookie_name'] . '_darkmode';
+		
+		$dark_case = $this->request->variable($cookie_darkmode_name, false, false, \phpbb\request\request_interface::COOKIE);
+		if ($dark_case)
 		{
-			$styleDoLight = "";
-			$styleDoDark = "display: none;";
+			$style_do_light = "";
+			$style_do_dark = "display: none;";
 			$class = "darkmode";
 		}
 		else
 		{
-			$styleDoLight = "display: none;";
-			$styleDoDark = "";
+			$style_do_light = "display: none;";
+			$style_do_dark = "";
 			$class = "lightmode";
 		}
-		
-		$this->template->append_var('BODY_CLASS', $class);
-		$this->template->append_var('STYLE_DO_DARK', $styleDoDark);
-		$this->template->append_var('STYLE_DO_LIGHT', $styleDoLight);
     
 		$this->user->add_lang_ext('aurelienazerty/darkmode', 'dark_mode');
-		$this->template->assign_var('DO_DARK_MESSAGE'	, $this->user->lang['DO_DARK_MODE']);
-		$this->template->assign_var('DO_LIGHT_MESSAGE'	, $this->user->lang['DO_LIGHT_MODE']);
+		
+		$this->template->assign_vars(array(
+			'BODY_CLASS'							=> 	$class,
+			'STYLE_DO_DARK'						=> 	$style_do_dark,
+			'STYLE_DO_LIGHT'					=> 	$style_do_light,
+			'DO_DARK_MESSAGE'					=> 	$this->user->lang['DO_DARK_MODE'],
+			'DO_LIGHT_MESSAGE'				=> 	$this->user->lang['DO_LIGHT_MODE'],
+			'S_COOKIE_DARKMODE_NAME'	=>	$cookie_darkmode_name,
+		));
 	}
 }
--- styles/all/theme/darkmode.js
+++ styles/all/theme/darkmode.js
@@ -5,12 +5,12 @@ function darkmode(doDark) {
 		$("#callLight").show();
 		var date = new Date();
 		date.setTime(date.getTime()+(365*24*60*60*1000));
-		document.cookie = "darkmode=true;expires=" + date.toGMTString() + ";path=/";
+		document.cookie = cookie_darkmode_name + "=true;expires=" + date.toGMTString() + ";path=/";
 	} else {
 		$("body").removeClass("darkmode");
 		$("#callDark").show();
 		$("#callLight").hide();
 		var date = new Date();
-		document.cookie = "darkmode=false;expires=" + date.toGMTString() + ";path=/";
+		document.cookie = cookie_darkmode_name + "=false;expires=" + date.toGMTString() + ";path=/";
 	}
 }
