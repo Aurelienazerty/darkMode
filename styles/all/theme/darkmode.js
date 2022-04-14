ccacf4c86e75752fc734a01dd19c43bbd9d993ef
Aurelienazerty <Aurelienazerty@users.noreply.github.com>
2022-04-14 16:50:39 +0200

    Update darkmode.js
    SameSite cookies
    
    https://developer.mozilla.org/fr/docs/Web/HTTP/Headers/Set-Cookie/SameSite

--- styles/all/theme/darkmode.js
+++ styles/all/theme/darkmode.js
@@ -6,13 +6,13 @@ function darkmode(doDark) {
 		$("#callLight").show();
 		var date = new Date();
 		date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
-		document.cookie = cookie_darkmode_name + "=true;expires=" + date.toGMTString() + ";path=/";
+		document.cookie = cookie_darkmode_name + "=true;expires=" + date.toGMTString() + ";path=/; Secure; SameSite=Lax";
 	} else {
 		$("html").addClass("lightmode");
 		$("html").removeClass("darkmode");
 		$("#callDark").show();
 		$("#callLight").hide();
 		var date = new Date();
-		document.cookie = cookie_darkmode_name + "=false;expires=" + date.toGMTString() + ";path=/";
+		document.cookie = cookie_darkmode_name + "=false;expires=" + date.toGMTString() + ";path=/; Secure; SameSite=Lax";
 	}
 }
